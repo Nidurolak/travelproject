d314f983698bfd2dd706bc84cce02bb49f9f9a35
Nidurolak <jaewoon08@gmail.com>
2023-02-25 19:19:42 +0900

    no message

---
+++ src/api/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// axios.create의 입력값으로 들어가는 객체는 configuration 객체에요.
+// https://axios-http.com/docs/req_config
+// 위 주소를 참고해주세요!
+const instance = axios.create({
+	baseURL: "http://sparta-kdh.kro.kr/api/",
+});
+
+export default instance;
