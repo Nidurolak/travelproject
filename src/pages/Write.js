129051fea11cf85f5a06197daa15a12fc85e773e
Nidurolak <jaewoon08@gmail.com>
2023-03-02 10:58:14 +0900

    asdasd

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { QueryClient, useMutation, useQuery } from 'react-query';
-import { useState } from 'react';
+import {  useState } from 'react';
 import { makePost } from "../api/Write";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from 'react-cookie'; 
@@ -11,6 +11,7 @@ import HeadBar from "../components/header/Header";
 
 
 function Write() {
+  const imageref = useRef();
   const [title, setTitle] = useState("")
   const [images, setImages] = useState("")
   const [content, setContent] = useState("")
@@ -22,6 +23,14 @@ function Write() {
   
   const ContentTextHandler = (event) =>{
     setContent(event.target.value)
+  }
+  const ImageHandler = (event) => {
+    const file = imageref.current.files[0] 
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () =>{
+      setImages(reader.result)
+    }
   }
 
   const mutate = useMutation(makePost)
@@ -46,13 +55,25 @@ function Write() {
     }
   }
 
-
   return (<>
   <HeadBar></HeadBar>
     <Container>
       <DetailContainer>
+        <DetailContentContainer>
         <DetailContentLeftBox>
-
+        <img src={images ? images :"/defaultInput.png"} alt="이미지를 가져와 주십시오"  style={{ width: "400px", height: "240px" }}/>
+        <input type="file" accept="image/jpeg, image/png" onChange={ImageHandler} ref = {imageref}></input>
+        
+        <DetailcontentSelectBox>
+        <h3>사용한 예산 </h3>
+        <select >
+          <option value="1">30만원 미만</option>
+          <option value="2">30 ~ 50만원</option>
+          <option value="3">50~ 70 만원</option>
+          <option value="4">70만원 초과</option>
+        </select>
+        </DetailcontentSelectBox>
+        
         </DetailContentLeftBox>
 
         
@@ -62,6 +83,7 @@ function Write() {
         <PutInput placeholder="300자 미만으로 적어주세요" maxLength={299}  onChange={ContentTextHandler}></PutInput>
         <DetailContentButton >작성하기</DetailContentButton>
         </DetailContentRightBox>
+        </DetailContentContainer>
 
       </DetailContainer>
 
@@ -99,45 +121,17 @@ background-color: rgb(230, 230, 230);
     margin-top: 15px;
   }
 `
-const NicknameAndDateBox = styled.div`
-  width: 520px;
-  height: 40px;
-  display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding-left: 10px;
-padding-right: 10px;
-`
-const DetailContentBox = styled.div`
-height: 180px;
-`
 
-const CommentInputBox = styled.div`
-padding-right: 10px;
-width: 1150px;
-max-width: 80%;
-height: 80px;
-border: 4px solid black;
-border-radius: 7px;
+const DetailContentContainer = styled.div`
 display: flex;
-justify-content: space-between;
+  flex: 1;
+flex-wrap: wrap;
 align-items: center;
+justify-content: flex-end;
+padding: 10px;
+background-color: rgb(210, 210, 210);
 `
-const CommentInput = styled.textarea`
-width: 80%;
-height: 75px ;
-background-color: wheat;
-border-radius: 5px;
-margin-left: 3px;
-box-sizing: border-box;
 
-text-align: left;
-vertical-align: top;
-white-space: pre-wrap;
-word-wrap: break-word;
-resize: none;
-`
 const PutInput = styled.textarea`
 width: 530px;
 height: 400px ;
@@ -166,67 +160,6 @@ white-space: pre-wrap;
 word-wrap: break-word;
 resize: none;
 `
-const CommentSubmit = styled.button`
-background-image : url(/check.jpg);
-border: 3px solid gold;
-border-radius: 5px;
-width: 50px;
-height: 50px;
-background-size: cover;
-`
-
-const DetailContentContainer = styled.div`
-display: flex;
-  flex: 1;
-flex-wrap: wrap;
-align-items: center;
-justify-content: flex-end;
-padding: 10px;
-background-color: rgb(210, 210, 210);
-`
-
-const DetailPutContainer = styled.div`
-display: flex;
-  flex: 1;
-flex-wrap: wrap;
-align-items: center;
-justify-content: flex-end;
-padding: 10px;
-background-color: rgb(210, 210, 210);
-`
-const InputContentLeftBox = styled.div`
-display: flex;
-  flex: 1;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width:550px;
-height: 400px;
-background-color: rgb(170, 170, 170);
-`
-const InputContentLeftImage = styled.div`
-background-image: url(${props => props.imageUrl});
-  width: 400px;
-  height: 240px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position:center center;
-  `
-const putContentLeftImage = styled.div`
-image{
-  width: 400px;
-  height: 240px;
-}
-  `
-const InputcontentLeftButtonBox = styled.div`
-  width: 400px;
-  height: 40px;
-  display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-  background-color: pink;
-  `
 
 const DetailContentLeftBox = styled.div`
 display: flex;
@@ -235,27 +168,20 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 width:550px;
-height: 400px;
+height: 500px;
 background-color: rgb(170, 170, 170);
 `
-
-const DetailContentLeftImage = styled.div`
-background-image: url(${props => props.imageUrl});
-  width: 400px;
-  height: 240px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position:center center;
-  `
-const DetailcontentLeftButtonBox = styled.div`
+const DetailcontentSelectBox = styled.div`
   width: 400px;
   height: 40px;
   display: flex;
 flex-direction: row;
-justify-content: space-between;
+justify-content: center;
 align-items: center;
+padding-left: 10px;
   background-color: pink;
   `
+
 const DetailContentButton = styled.button`
 width: 80px;
 height: 40px;
@@ -269,7 +195,7 @@ flex-direction: column;
 justify-content: flex-start;
 align-items: center;
 width: 550px;
-height: 400px;
+height: 500px;
 background-color: rgb(170, 170, 170);
 > * {
   margin-top: 10px;
@@ -277,46 +203,5 @@ background-color: rgb(170, 170, 170);
   text-align: left;
 }
 `
-
-const DetailRepleBox = styled.div`
-padding-right: 10px;
-width: 650px;
-height: 120px;
-border: 4px solid black;
-border-radius: 7px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-`
-const RepleReftContainer = styled.div`
-width: 570px;
-height: 120px;
-display: flex;
-flex-direction: column;
-justify-content: flex-start;
-`
-const RepleComment = styled.div`
-background-color: ${props => props.bg};
-padding-left: 10px;
-padding-right: 10px;
-width: ${props => props.width}px;
-height: ${props => props.height}px;
-font-size: ${props => props.fontsize}px;
-overflow: hidden;
-white-space: normal;
-word-break: break-word;
-text-overflow: ellipsis;
-display: flex;
-justify-content: space-between;
-`
-const RepleDeleteBox = styled.button`
-background-image : url(/delete.png);
-border: 3px solid gold;
-border-radius: 5px;
-width: 50px;
-height: 50px;
-background-size: cover;
-`
-
 
 export default Write;
